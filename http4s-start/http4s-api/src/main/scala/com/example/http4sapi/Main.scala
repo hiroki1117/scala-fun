f4917dde045a8a1ca130b198f4b9da4bf51b94fa
hiroki ohno <scala1117@gmail.com>
2019-07-19 13:34:38 +0900

    add http4s

---
+++ http4s-start/http4s-api/src/main/scala/com/example/http4sapi/Main.scala
@@ -0,0 +1,9 @@
+package com.example.http4sapi
+
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.implicits._
+
+object Main extends IOApp {
+  def run(args: List[String]) =
+    Http4sapiServer.stream[IO].compile.drain.as(ExitCode.Success)
+}
